62f0736da62a0c3716664a2143448a54095b005e
Crowdbotics <team@crowdbotics.com>
2021-12-18 19:25:48 +0000

    Update admin title

--- backend/wediliva_32482/urls.py
+++ backend/wediliva_32482/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "WeDiliva"
-admin.site.site_title = "WeDiliva Admin Portal"
-admin.site.index_title = "WeDiliva Admin"
+admin.site.site_header = "App Name"
+admin.site.site_title = "App Name Admin Portal"
+admin.site.index_title = "App Name Admin"
 
 # swagger
 api_info = openapi.Info(
